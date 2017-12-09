08a6e1fd38ae26a564af49c024b30fd5de83607e
dengzhiqiang <1445124133@qq.com>
2017-12-09 12:06:40 +0800

    '视口的宽度和高度'

--- modules/dom.js
+++ modules/dom.js
@@ -1,3 +1,17 @@
 var dom = (function (dom) {
-    
+    /**
+     * 获取视口的宽度和高度
+     * @returns {{width: (Number|number), height: (Number|number)}}
+     */
+    var getViewportSize = function () {
+        return {
+            width: window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
+            height: window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
+        };
+    };
+
+    dom.getViewportSize = getViewportSize;
+
+    return dom;
+
 })(window.dom || {});
